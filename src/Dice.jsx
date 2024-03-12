8b311d083b4f5874c5089593554750786ddc9775
mohit <mohitgujjar2121@gmail.com>
2024-03-12 10:19:45 +0530

    changed div to button

--- src/Dice.jsx
+++ src/Dice.jsx
@@ -4,9 +4,9 @@ let styles={
     backgroundColor:props.value.isHeld?"#5ff694":"white"
     };
     return(
-        <div className="dice-face" style={styles} onClick={props.heldData}>
+        <button className="dice-face" style={styles} onClick={props.heldData}>
             <h2 className="dice-value">{props.value.value}</h2>
-        </div>
+        </button>
     );
 }
 export default Dice;
